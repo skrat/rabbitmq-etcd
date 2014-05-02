b79f4d9209050a0b0e3b1667ea448e8952f64a63
Dusan Maliarik <dusan.maliarik@gmail.com>
2014-05-02 19:20:27 +0200

    Adding support for user tagging.

--- README.md
+++ README.md
@@ -10,15 +10,19 @@ Installation
 ------------
 
 With docker:
-```sh
-/usr/bin/docker run --env-file /etc/environment -p 5672:5672 -p 15672:15672 skrat/rabbitmq-etcd
+```
+/usr/bin/docker run                 \
+        --env-file /etc/environment \
+        -p 5672:5672                \
+        -p 15672:15672              \
+        skrat/rabbitmq-etcd
 ```
 
 Synchronization script expects `COREOS_PRIVATE_IPV4` variable to be host's
 IP address. It uses it access etcd, and publish its own location to etcd.
 
 With fleet:
-```sh
+```
 git clone https://github.com/skrat/rabbitmq-etcd.git
 fleetctl start rabbitmq-etcd/rabbitmq.service
 ```
@@ -27,26 +31,38 @@ Usage
 -----
 
 Creating vhost:
-```sh
+
+```
 etcdctl set /rabbitmq/vhosts/example 1
 ```
 
 Deleting vhost:
-```sh
+
+```
 etcdctl rm /rabbitmq/vhosts/example
 ```
 
 Creating user:
-```sh
+
+```
+etcdctl set /rabbitmq/users/john secret
+```
+
+Creating user with tags:
+
+```
+etcdctl set /rabbitmq/tags/john management,administrator
 etcdctl set /rabbitmq/users/john secret
 ```
 
 Deleting user:
-```sh
+
+```
 etcdctl rm /rabbitmq/users/john
 ```
 
 Setting permissions:
-```sh
+
+```
 etcdctl set /rabbitmq/permissions/example/john ".*/.*/.*"
 ```
--- configsync.py
+++ configsync.py
@@ -12,14 +12,22 @@ from collections import defaultdict
 
 PREFIX = '/rabbitmq'
 
+def filter_prefix(d, pref):
+    return [k for k in d.keys() if k.startswith("%s/%s/" % (PREFIX, pref))]
+
+class log:
+    @staticmethod
+    def info(msg):
+        print("=> " + msg)
+    @staticmethod
+    def error(msg, e=None):
+        print("=! %s : %s" % (msg, str(e)), file=sys.stderr)
+
 class State:
     def __init__(self):
         self.vhosts = set()
         self.users = set()
         self.permissions = defaultdict(lambda: defaultdict(lambda: None))
-
-def filter_prefix(d, pref):
-    return [k for k in d.keys() if k.startswith("%s/%s/" % (PREFIX, pref))]
 
 class Config:
     def __init__(self):
@@ -31,68 +39,70 @@ class Config:
         self.state.vhosts.update(fresh_vhosts)
 
     def sync(self, children):
-        d = dict((child.key, child.value) for child in children)
+        etc = dict((child.key, child.value) for child in children)
 
-        vhosts = set(k.split('/')[-1] for k in filter_prefix(d, 'vhosts'))
+        vhosts = set(k.split('/')[-1] for k in filter_prefix(etc, 'vhosts'))
         self.sync_vhosts(vhosts)
 
-        users = dict((k.split('/')[-1], d[k]) for k in filter_prefix(d, 'users'))
-        self.sync_users(users)
+        users = dict((k.split('/')[-1], etc[k]) for k in filter_prefix(etc, 'users'))
+        self.sync_users(users, etc)
 
-        permissions = [tuple(k.split('/')[-2:] + [d[k]]) for k in filter_prefix(d, 'permissions')]
+        permissions = [tuple(k.split('/')[-2:] + [etc[k]]) for k in filter_prefix(etc, 'permissions')]
         self.sync_permissions(permissions)
 
     def sync_vhosts(self, vhosts):
         vhosts_to_add = vhosts - self.state.vhosts
         for vhost in vhosts_to_add:
-            print("=> Creating vhost %s" % vhost)
+            log.info("Creating vhost %s" % vhost)
             try:
                 self.client.create_vhost(vhost)
                 self.state.vhosts.add(vhost)
             except Exception as e:
-                print("=! Failed to create vhost %s - %s" % (vhost, str(e)), file=sys.stderr)
+                log.error("Failed to create vhost %s" % vhost, e)
         vhosts_to_rm = self.state.vhosts - vhosts - {'/'}
         for vhost in vhosts_to_rm:
-            print("=> Removing vhost %s" % vhost)
+            log.info("Removing vhost %s" % vhost)
             try:
                 self.client.delete_vhost(vhost)
                 self.state.vhosts.remove(vhost)
             except Exception as e:
-                print("=! Failed to remove vhost %s - %s" % (vhost, str(e)), file=sys.stderr)
+                log.error("Failed to remove vhost %s" % vhost, e)
 
-    def sync_users(self, users):
+    def sync_users(self, users, etc):
         user_set = set(users.keys())
         users_to_add = user_set - self.state.users
         for user in users_to_add:
-            print("=> Creating user %s:%s" % (user, users[user]))
+            tags = etc.get(PREFIX + '/tags/' + user, '')
+            log.info("Creating user %s:%s with tags" % (user, users[user], tags))
             try:
-                self.client.create_user(user, users[user])
+                self.client.create_user(user, users[user], tags)
                 self.state.users.add(user)
             except Exception as e:
-                print("=! Failed to create user %s - %s" % (user, str(e)), file=sys.stderr)
+                log.error("Failed to create user %s" % user, e)
         users_to_rm = self.state.users - user_set - {'guest'}
         for user in users_to_rm:
-            print("=> Removing user %s" % user)
+            log.info("Removing user %s" % user)
             try:
                 self.client.delete_user(user)
                 self.state.users.remove(user)
             except Exception as e:
-                print("=! Failed to remove user %s - %s" % (user, str(e)), file=sys.stderr)
+                log.error("Failed to remove user %s" % user, e)
 
     def sync_permissions(self, permissions):
         for (vhost, user, perm) in permissions:
             if self.state.permissions[vhost][user] != perm:
-                print("=> Setting permissions for %s on %s to %s" % (vhost, user, perm))
+                log.info("Setting permissions for %s on %s to %s" % (vhost, user, perm))
                 try:
                     self.client.set_vhost_permissions(vhost, user, *perm.split('/'))
                     self.state.permissions[vhost][user] = perm
                 except Exception as e:
-                    print("=! Failed to set permissions %s" % str(e), file=sys.stderr)
+                    log.error("Failed to set permissions for %s" % user, e)
 
 if __name__ == '__main__':
     host_ip = os.environ.get('COREOS_PRIVATE_IPV4', None)
-    print("=> Connecting to etcd at %s" % host_ip)
-    init   = False
+    log.info("Connecting to etcd at %s" % host_ip)
+
+    init = False
     client = etcd.Client(host=host_ip, port=4001)
     config = Config()
 
